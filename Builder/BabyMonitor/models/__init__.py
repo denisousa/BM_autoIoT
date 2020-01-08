961c2ac47a5d351485d7eec3b1984c6e3500fe8e
adrilene <adrilene.fonseca@aluno.uece.br>
2020-01-08 15:09:16 -0300

    adding new devices. erro db

--- Builder/BabyMonitor/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Monitor import *
-
-from .MonitorBreathingSensor import *
-from .MonitorBreathingSensorData import *
-from .MonitorSleepingSensor import *
-from .MonitorSleepingSensorData import *
-from .MonitorCryingSensor import *
-from .MonitorCryingSensorData import *
-
